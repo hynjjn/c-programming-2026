11c2340cece404725b3376e2b02310e6dcb63577
Hyeonjin Seon <37838569+hynjjn@users.noreply.github.com>
2026-03-20 02:10:39 +0000

    product_name -> name

--- 20260320_stdio/exercise.c
+++ 20260320_stdio/exercise.c
@@ -2,12 +2,12 @@
 
 int main() {
     printf("=== 상품 정보 입력 ===\n");
-    char product_name[10];
+    char name[10];
     int price, quantity;
     
     // getter
     printf("상품명: ");
-    scanf("%s", product_name);
+    scanf("%s", name);
     printf("단가: ");
     scanf("%d", &price);
     printf("수량: ");
@@ -16,7 +16,6 @@ int main() {
     // print
     printf("=== 발행된 영수증 ===\n");
     printf("%-10s %-10s %-10s\n", "name", "price", "quantity");
-    
     printf("%-10s %-10d %-10d\n", product_name, price, quantity);
     printf("==============================\n");
     return 0;
