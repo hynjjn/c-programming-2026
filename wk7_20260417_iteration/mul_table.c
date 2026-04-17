b771ee17615ed284ef2da3f35655b73e8d7f6036
hj <hj@rocky-rig.queue-pauling.ts.net>
2026-04-17 10:50:41 +0900

    0417

--- wk7_20260417_iteration/mul_table.c
+++ wk7_20260417_iteration/mul_table.c
@@ -3,7 +3,7 @@
 int main() {
     for (int i = 1; i < 10; i++) {
         for (int j = 1; j < 10; j++) {
-            printf("%d * %d = %2d\t", i, j, i*j);
+            printf("%d * %d = %2d\t", j, i, j*i);
         }
         printf("\n");
     }
